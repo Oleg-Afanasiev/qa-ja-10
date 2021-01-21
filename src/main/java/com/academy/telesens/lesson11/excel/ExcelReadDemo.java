f8cbc1cfcd247a330f2281d3c5ccdb9e7174192a
af_oleg <oleg.kh81@gmail.com>
2021-01-21 19:27:31 +0200

    lesson 11 - excel read demo

--- src/main/java/com/academy/telesens/lesson11/excel/ExcelReadDemo.java
+++ src/main/java/com/academy/telesens/lesson11/excel/ExcelReadDemo.java
@@ -4,33 +4,33 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class ExcelReadDemo {
     public static void main(String[] args) {
-        Workbook workbook= new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet();
-        Row row = sheet.createRow(0);
-        Cell cell1 = row.createCell(0);
-        Cell cell2 = row.createCell(1);
-
-        cell1.setCellValue("Hello");
-        cell2.setCellValue("World!");
-
-        // запись на диск
-        String path = "C:/Users/af_oleg/Documents/QA_JA_10/data/demo.xslx";
+        String path = "C:/Users/af_oleg/Documents/QA_JA_10/data/demo.xlsx";
         try {
-            OutputStream os = new FileOutputStream(path);
-            workbook.write(os);
+            FileInputStream fis = new FileInputStream(path);
+            Workbook workbook = new XSSFWorkbook(fis);
+            Sheet sheet = workbook.getSheetAt(0);
+            Row row = sheet.getRow(0);
+            Cell cell1 = row.getCell(0);
+            Cell cell2 = row.getCell(1);
+            Cell cell3 = row.getCell(2);
+
+            System.out.println(
+                    cell1.getStringCellValue() + " " +
+                            cell2.getStringCellValue() + " " +
+                            cell3.getNumericCellValue()
+            );
             workbook.close();
-            os.close();
+            fis.close();
         } catch (IOException e) {
-            System.out.println("Ошибка записи");
-            System.out.println(e);
+            System.out.println("Ошибка " + e);
         }
     }
 }
---
+++ src/main/java/com/academy/telesens/lesson11/excel/ExcelWriteDemo.java
@@ -0,0 +1,38 @@
+package com.academy.telesens.lesson11.excel;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ExcelWriteDemo {
+    public static void main(String[] args) {
+        Workbook workbook= new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet();
+        Row row = sheet.createRow(0);
+        Cell cell1 = row.createCell(0);
+        Cell cell2 = row.createCell(1);
+        Cell cell3 = row.createCell(2);
+
+        cell1.setCellValue("Hello");
+        cell2.setCellValue("World!");
+        cell3.setCellValue(25);
+
+        // запись на диск
+        String path = "C:/Users/af_oleg/Documents/QA_JA_10/data/demo.xlsx";
+        try {
+            OutputStream os = new FileOutputStream(path);
+            workbook.write(os);
+            workbook.close();
+            os.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка записи");
+            System.out.println(e);
+        }
+    }
+}
