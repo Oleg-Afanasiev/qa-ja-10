68a794a94f2bf59e2f7f72e8abf950c60f591400
af_oleg <oleg.kh81@gmail.com>
2021-01-23 10:39:50 +0200

    lesson 13 - property ver2

--- src/main/java/com/academy/telesens/util/PropertyProvider.java
+++ src/main/java/com/academy/telesens/util/PropertyProvider.java
@@ -8,6 +8,7 @@ public class PropertyProvider {
     private static final String DEFAULT_PROP_FILE = "default-cfg.properties";
     private static Properties prop;
 
+    // блок статической инициализации
     static {
         init();
     }
@@ -16,13 +17,14 @@ public class PropertyProvider {
         try {
             prop = new Properties();
             InputStream is = PropertyProvider.class.getClassLoader().getResourceAsStream(DEFAULT_PROP_FILE);
-            prop.load(is);
+            prop.load(is); // загружаем физический файл с пропертями
         } catch (IOException e) {
             System.out.println("Error prop initialization");
             e.printStackTrace();
         }
     }
 
+    // метод отдает проперти по ключу
     public static String get(String key) {
        return prop.getProperty(key);
     }
