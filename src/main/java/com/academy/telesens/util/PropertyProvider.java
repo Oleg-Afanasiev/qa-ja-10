c35bce7d3389de3ba5832e1b02a3d0b2a1475095
af_oleg <oleg.kh81@gmail.com>
2021-01-23 10:31:07 +0200

    lesson 13 - property

--- src/main/java/com/academy/telesens/util/PropertyProvider.java
+++ src/main/java/com/academy/telesens/util/PropertyProvider.java
@@ -1,23 +1,29 @@
 package com.academy.telesens.util;
 
-import com.academy.telesens.lesson12.MySQLDemo;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertyProvider {
     private static final String DEFAULT_PROP_FILE = "default-cfg.properties";
-    public static String get(String key) {
+    private static Properties prop;
+
+    static {
+        init();
+    }
+
+    private static void init() {
         try {
+            prop = new Properties();
             InputStream is = PropertyProvider.class.getClassLoader().getResourceAsStream(DEFAULT_PROP_FILE);
-            Properties prop = new Properties();
             prop.load(is);
-            String value = prop.getProperty(key);
-            return value;
         } catch (IOException e) {
-            System.out.println(e);
-            return null;
+            System.out.println("Error prop initialization");
+            e.printStackTrace();
         }
+    }
+
+    public static String get(String key) {
+       return prop.getProperty(key);
     }
 }
