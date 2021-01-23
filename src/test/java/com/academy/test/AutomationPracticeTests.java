b0c2da62c146ba2b70dbe0e8ebb1314d20d9ba47
af_oleg <oleg.kh81@gmail.com>
2021-01-23 12:11:42 +0200

    lesson 13 - assertions

--- src/test/java/com/academy/test/AutomationPracticeTests.java
+++ src/test/java/com/academy/test/AutomationPracticeTests.java
@@ -1,34 +1,32 @@
 package com.academy.test;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-
 import com.academy.telesens.util.PropertyProvider;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import java.util.concurrent.TimeUnit;
 
 public class AutomationPracticeTests {
     private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
+    private String baseUrl = "http://automationpractice.com/index.php";
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", PropertyProvider.get("driver.chrome"));
         System.setProperty("webdriver.gecko.driver", PropertyProvider.get("driver.firefox"));
         driver = new FirefoxDriver();
-        baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    public void testUntitledTestCase() throws Exception {
-        driver.get("http://automationpractice.com/index.php");
+    public void testAuth() throws Exception {
+        driver.get(baseUrl);
         driver.findElement(By.linkText("Sign in")).click();
         driver.findElement(By.id("email")).click();
         driver.findElement(By.id("email")).clear();
@@ -37,47 +35,14 @@ public class AutomationPracticeTests {
         driver.findElement(By.id("passwd")).clear();
         driver.findElement(By.id("passwd")).sendKeys("pwd");
         driver.findElement(By.xpath("//button[@id='SubmitLogin']/span")).click();
+
+        String errMsgExpected = "Invalid email address.1";
+        String errMsgActual = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div[1]/ol/li")).getText();
+        Assert.assertEquals(errMsgActual, errMsgExpected);
     }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
         driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
     }
 }
