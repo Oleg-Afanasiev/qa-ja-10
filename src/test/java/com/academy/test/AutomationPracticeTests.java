a7ce4d7bfa2ac8dc061d5faee83d3c35dc42b3ee
af_oleg <oleg.kh81@gmail.com>
2021-01-28 19:28:06 +0200

    lesson 14 - selenium driver

--- src/test/java/com/academy/test/AutomationPracticeTests.java
+++ src/test/java/com/academy/test/AutomationPracticeTests.java
@@ -3,7 +3,11 @@ package com.academy.test;
 import com.academy.telesens.util.PropertyProvider;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -25,9 +29,10 @@ public class AutomationPracticeTests {
     }
 
     @Test
-    public void testAuth() throws Exception {
+    public void testAuth() {
         driver.get(baseUrl);
-        driver.findElement(By.linkText("Sign in")).click();
+        WebElement elSignIn = driver.findElement(By.linkText("Sign in"));
+        elSignIn.click();
         driver.findElement(By.id("email")).click();
         driver.findElement(By.id("email")).clear();
         driver.findElement(By.id("email")).sendKeys("username");
@@ -39,6 +44,11 @@ public class AutomationPracticeTests {
         String errMsgExpected = "Invalid email address.1";
         String errMsgActual = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div[1]/ol/li")).getText();
         Assert.assertEquals(errMsgActual, errMsgExpected);
+    }
+
+    @Test
+    public void testWomenCategory() {
+
     }
 
     @AfterClass(alwaysRun = true)
