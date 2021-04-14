a50d044829d9b48f4716fc4df89a279629e9b4fe
oleg <oleg.kh81@gmail.com>
2021-04-14 12:46:32 +0300

    refactor - add comments to 'addSubscriber' test

--- src/test/java/com/academy/selenide/SelenideTests.java
+++ src/test/java/com/academy/selenide/SelenideTests.java
@@ -20,13 +20,13 @@ import java.util.List;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTests {
-    private String baseUrl = "http://localhost:8081";
+    private static final String BASE_URL = "http://localhost:8081";
 
     @BeforeClass
     public void beforeClass() {
-        Configuration.browser = "chrome";
-        Configuration.timeout = 10;
-        Configuration.startMaximized = true;
+        Configuration.browser = "chrome"; // запуск в браузере "chrome"
+        Configuration.timeout = 10;  // настройка таймаута неявных ожиданий (10 сек)
+        Configuration.startMaximized = true; // при старте распахиваем окно на весь экран
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
@@ -36,34 +36,39 @@ public class SelenideTests {
 
     @Test(dataProvider = "subscriberProvider")
     public void testAddSubscriber(Subscriber expectedSubscriber) {
-        HomePage homePage = open(baseUrl, HomePage.class);
-        SubscribersPage subscribersPage = homePage.goToSubscriber();
-        List<Subscriber> before = subscribersPage.getAllSubscribers();
+        // Этот тест использует паттерн PageObject
+        // Данные для теста получаем от провайдера "subscriberProvider"
+        // 'expectedSubscriber' - эталонный абонента, кот. создаем
+        HomePage homePage = open(BASE_URL, HomePage.class);  // открываем страницу по url и дальше используем HomePage
+        SubscribersPage subscribersPage = homePage.goToSubscriber(); // переходим на страницу абонентов
+        List<Subscriber> before = subscribersPage.getAllSubscribers(); // читаем список всех абонентов со страницы
 
+        // Здесь хардкод - нужно разложить по логику по классам Page
         // add new subscriber
-        $(By.id("add")).click();
-        $(By.id("fname")).setValue(expectedSubscriber.getFirstName());
-        $(By.id("lname")).setValue(expectedSubscriber.getLastName());
-        if (expectedSubscriber.getGender() == Gender.MALE) {
+        $(By.id("add")).click(); // Нажимаем кнопку добавить
+        $(By.id("fname")).setValue(expectedSubscriber.getFirstName()); // вводим имя
+        $(By.id("lname")).setValue(expectedSubscriber.getLastName()); // вводим фамилию
+        if (expectedSubscriber.getGender() == Gender.MALE) { // нажимаем пол
             $(By.id("MALE")).click();
         } else {
             $(By.id("FEMALE")).click();
         }
-        $(By.id("age")).setValue(String.valueOf(expectedSubscriber.getAge()));
+        $(By.id("age")).setValue(String.valueOf(expectedSubscriber.getAge())); // заполняем возраст
         $("body > div > form > button").click();
 
-        Subscriber actualSubscriber = subscribersPage.getLastSubscriber();
-        expectedSubscriber.setId(actualSubscriber.getId());
-        Assert.assertEquals(actualSubscriber, expectedSubscriber);
+        // хардкод закончился - используем паттерн page
+        Subscriber actualSubscriber = subscribersPage.getLastSubscriber(); // получаем последнего абонента из списка
+        expectedSubscriber.setId(actualSubscriber.getId()); // записываем id в эталонный абонент (id был сгенерирован автоматически при создании)
+        Assert.assertEquals(actualSubscriber, expectedSubscriber); // сравниваем созданный абонент с эталонным по всем полям (в т.ч. по id)
 
-        List<Subscriber> after = subscribersPage.getAllSubscribers();
-        Assert.assertEquals(after.size(), before.size() + 1);
-        before.add(actualSubscriber);
+        List<Subscriber> after = subscribersPage.getAllSubscribers(); // получаем список всех абонентов после создания
+        Assert.assertEquals(after.size(), before.size() + 1); // проверяем, что размер списка стал на единицу больше
+        before.add(actualSubscriber); // добавляем в список, полученный до создания созданного абоннета
         // sort by id
-        before.sort(Comparator.comparingInt(Subscriber::getId));
+        before.sort(Comparator.comparingInt(Subscriber::getId)); // сортируем списки по id
         after.sort(Comparator.comparingInt(Subscriber::getId));
 
-        Assert.assertEquals(after, before);
+        Assert.assertEquals(after, before); // теперь проверям, что списки до и после - одинаковые
     }
 
     @AfterClass
@@ -73,6 +78,7 @@ public class SelenideTests {
 
     @DataProvider(name = "subscriberProvider")
     public Object[][] subscriberProvider() {
+        // подготавливаем данные для нового абонента
         Subscriber subscriber = new Subscriber();
         subscriber.setFirstName("testFirstName");
         subscriber.setLastName("testLastName");
--- src/test/java/com/academy/selenide/page/SubscribersPage.java
+++ src/test/java/com/academy/selenide/page/SubscribersPage.java
@@ -12,14 +12,21 @@ import java.util.List;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SubscribersPage {
+    // шаблоны xpath локаторов для получения имени/фамилии/возраста/пола по id
+    // id будет подставляться вместо %d
+    private static final String F_NAME_BY_ID_X_PATH_TMPL = "//tr[td/a[text() = '%d']]/td[3]";
+    private static final String L_NAME_BY_ID_X_PATH_TMPL = "//tr[td/a[text() = '%d']]/td[4]";
+    private static final String AGE_NAME_BY_ID_X_PATH_TMPL = "//tr[td/a[text() = '%d']]/td[5]";
+    private static final String GENDER_NAME_BY_ID_X_PATH_TMPL = "//tr[td/a[text() = '%d']]/td[6]";
+
     @FindBy(css = "a[name='contact-edit-id']")
-    private List<SelenideElement> idS;
+    private List<SelenideElement> idS; // список всех id из списка абонентов
 
     @FindBy(css = "tr > td:nth-child(3)")
-    private List<SelenideElement> fNames;
+    private List<SelenideElement> fNames; // список всех имен из списка абонентов
 
     @FindBy(css = "tr > td:nth-child(4)")
-    private List<SelenideElement> lNames;
+    private List<SelenideElement> lNames; // список всех имен из списка абонентов
 
     @FindBy(css = "tr > td:nth-child(5)")
     private List<SelenideElement> ageNames;
@@ -28,10 +35,10 @@ public class SubscribersPage {
     private List<SelenideElement> genderNames;
 
     @FindBy(css = "body > div > div:nth-child(4) > div > table > tbody > tr:last-child > td:nth-child(2)")
-    private SelenideElement lastId;
+    private SelenideElement lastId; // id последней записи в списке
 
     @FindBy(css = "body > div > div:nth-child(4) > div > table > tbody > tr:last-child > td:nth-child(3)")
-    private SelenideElement lastFistName;
+    private SelenideElement lastFistName; // имя последней записи в списке
 
     @FindBy(css = "body > div > div:nth-child(4) > div > table > tbody > tr:last-child > td:nth-child(4)")
     private SelenideElement lastLastName;
@@ -42,18 +49,14 @@ public class SubscribersPage {
     @FindBy(css = "body > div > div:nth-child(4) > div > table > tbody > tr:last-child > td:nth-child(6)")
     private SelenideElement lastGender;
 
-    private String fNameByIdXPathTempl = "//tr[td/a[text() = '%d']]/td[3]";
-    private String lNameByIdXPathTempl = "//tr[td/a[text() = '%d']]/td[4]";
-    private String ageNameByIdXPathTempl = "//tr[td/a[text() = '%d']]/td[5]";
-    private String genderNameByIdXPathTempl = "//tr[td/a[text() = '%d']]/td[6]";
-
     public List<Subscriber> getAllSubscribers() {
+        // формируем список абонентов из списков со значениями id/имени/фамилии...
         List<Subscriber> subscribers = new ArrayList<>();
-        for (int i = 0; i < idS.size(); i++) {
-            Subscriber subscriber = new Subscriber();
-            subscriber.setId(Integer.parseInt(idS.get(i).getText().trim()));
-            subscriber.setFirstName(fNames.get(i).text().trim());
-            subscriber.setLastName(lNames.get(i).text().trim());
+        for (int i = 0; i < idS.size(); i++) { // цикл по элементам списка
+            Subscriber subscriber = new Subscriber(); // создаем объект абонент
+            subscriber.setId(Integer.parseInt(idS.get(i).getText().trim())); // записываем id из списка
+            subscriber.setFirstName(fNames.get(i).text().trim()); // записываем имя из списка
+            subscriber.setLastName(lNames.get(i).text().trim()); // ...
             subscriber.setAge(Integer.parseInt(ageNames.get(i).text().trim()));
             subscriber.setGender(Gender.parse(genderNames.get(i).text().trim()));
 
@@ -64,6 +67,7 @@ public class SubscribersPage {
     }
 
     public Subscriber getLastSubscriber() {
+        // создаем абонента последнего по списку, используя соотв. локаторы
         Subscriber subscriber = new Subscriber();
 
         subscriber.setId(Integer.parseInt(lastId.text().trim()));
@@ -76,10 +80,11 @@ public class SubscribersPage {
     }
 
     public Subscriber getSubscriberById(int id) {
-        String fName = $(By.xpath(String.format(fNameByIdXPathTempl, id))).text();
-        String lName = $(By.xpath(String.format(lNameByIdXPathTempl, id))).text();
-        String ageName = $(By.xpath(String.format(ageNameByIdXPathTempl, id))).text();
-        String genderName = $(By.xpath(String.format(genderNameByIdXPathTempl, id))).text();
+        // создаем абонента по id, используя локаторы шаблоны, в кот. нужно id подставлять
+        String fName = $(By.xpath(String.format(F_NAME_BY_ID_X_PATH_TMPL, id))).text();
+        String lName = $(By.xpath(String.format(L_NAME_BY_ID_X_PATH_TMPL, id))).text();
+        String ageName = $(By.xpath(String.format(AGE_NAME_BY_ID_X_PATH_TMPL, id))).text();
+        String genderName = $(By.xpath(String.format(GENDER_NAME_BY_ID_X_PATH_TMPL, id))).text();
         Subscriber subscriber = new Subscriber();
         subscriber.setFirstName(fName);
         subscriber.setLastName(lName);
